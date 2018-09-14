81d5818fa43d2e9defdc02e80a29e1ca484b623e
Kudaybergen <tuleubekov.kas@gmail.com>
2018-09-14 18:23:22 +0600

    Lifecycle log

--- app/src/main/java/com/akai/geektech/lesson4/MainActivity.java
+++ app/src/main/java/com/akai/geektech/lesson4/MainActivity.java
@@ -1,13 +1,89 @@
 package com.akai.geektech.lesson4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "LifecycleMain";
+    private TextView textView1;
+    private Button buttonGO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        init();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState " + savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+
+    private void btnClickedGoToSecondActivity() {
+        Log.d(TAG, "btn clicked");
+        startActivity(new Intent(this, SecondActivity.class));
+    }
+
+    private void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_startSecondActivity: btnClickedGoToSecondActivity(); break;
+        }
+    }
+
+    private void init() {
+        textView1 = findViewById(R.id.textView1);
+        buttonGO = findViewById(R.id.btn_startSecondActivity);
+        buttonGO.setOnClickListener(this::onClick);
     }
 }
